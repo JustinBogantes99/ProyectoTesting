0fd4f5c2d1e86011109d4d9756f7b0fff53aa781
Calo <calo@estudiantec.cr>
2021-11-17 20:49:29 -0600

    prueba 20

--- mochaTest/test/registrarse.spec.js
+++ mochaTest/test/registrarse.spec.js
@@ -1,7 +1,6 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 const expect = chai.expect;
-var should = require('chai').should();
 
 chai.use(chaiHttp);
 const url= 'http://localhost:3000';
@@ -12,7 +11,6 @@ describe('Iniciar sesion: ', ()=>{
         .post('/inicio_sesion')
         .send({correoInicio:'prueba@gmail.com',contrasenaInicio: 'aA1234'})
         .end( function(err,res){
-            console.log(res.body)
             expect(res).to.redirectTo('http://localhost:3000/inicio');
             done();
         });
@@ -22,7 +20,6 @@ describe('Iniciar sesion: ', ()=>{
         .post('/inicio_sesion')
         .send({correoInicio:'prueba@gmail.com',contrasenaInicio: '123456'})
         .end( function(err,res){
-            console.log(res.body)
             expect(res).to.redirectTo('http://localhost:3000/inicio_sesion');
             done();
         });
@@ -32,9 +29,56 @@ describe('Iniciar sesion: ', ()=>{
         .post('/inicio_sesion')
         .send({correoInicio:'aaaa@gmail.com',contrasenaInicio: '123456'})
         .end( function(err,res){
-            console.log(res.body)
             expect(res).to.redirectTo('http://localhost:3000/inicio_sesion');
             done();
         });
     });
+});
+
+describe('Registrarse: ', ()=>{
+    
+    it('información correcta', (done) => {
+        let r = (Math.random() + 1).toString(36).substring(7);
+        let correo = r + '@gmail.com';
+        chai.request(url)
+        .post('/registro_usuario')
+        .type('form')
+        .send({nombre:'prueba', apellido1: 'prueba', apellido2: 'prueba', telefono: '1234-1234', correo: correo, contrasena: 'aA1234', confirma_contrasena: 'aA1234'})
+        .end( function(err,res){
+            expect(res).to.redirectTo('http://localhost:3000/inicio_sesion');
+            done();
+        });
+    });
+    
+    it('correo en uso', (done) => {
+        chai.request(url)
+        .post('/registro_usuario')
+        .type('form')
+        .send({nombre:'prueba', apellido1: 'prueba', apellido2: 'prueba', telefono: '1234-1234', correo: 'eduardj.com@gmail.com', contrasena: 'aA1234', confirma_contrasena: 'aA1234'})
+        .end( function(err,res){
+            expect(res).to.not.redirect;
+            done();
+        });
+    });
+    it('contraseñas diferentes', (done) => {
+        chai.request(url)
+        .post('/registro_usuario')
+        .type('form')
+        .send({nombre:'prueba', apellido1: 'prueba', apellido2: 'prueba', telefono: '1234-1234', correo: 'eduardj.com@gmail.com', contrasena: 'aaA12345', confirma_contrasena: 'aA1234'})
+        .end( function(err,res){
+            expect(res).to.not.redirect;
+            done();
+        });
+    });
+    it('contraseña no cumple requisitos', (done) => {
+        chai.request(url)
+        .post('/registro_usuario')
+        .type('form')
+        .send({nombre:'prueba', apellido1: 'prueba', apellido2: 'prueba', telefono: '1234-1234', correo: 'eduardj.com@gmail.com', contrasena: 'aaaa', confirma_contrasena: 'aaaa'})
+        .end( function(err,res){
+            expect(res).to.not.redirect;
+            done();
+        });
+    });
+
 });
